a0ae5bc603c8f007209d85d0903287a18a7b3b97
Azat S. <to@azat.io>
2025-08-20 23:05:50 +0300

    feat!: move to esm only

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,34 @@
 import { prettierFormat } from 'vite-plugin-prettier-format'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import fs from 'node:fs/promises'
 import path from 'node:path'
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: [path.resolve(import.meta.dirname, 'index.ts')],
+      fileName: (_format, entryName) => `${entryName}.js`,
+      name: 'eslint-plugin-de-morgan',
+      formats: ['es'],
+    },
     rollupOptions: {
-      onwarn: (warning, warn) => {
-        let suppressedCodes = ['MIXED_EXPORTS']
-
-        if (!suppressedCodes.includes(warning.code ?? '')) {
-          warn(warning)
-        }
-      },
+      external: (id: string) => !id.startsWith('.') && !path.isAbsolute(id),
       output: {
         preserveModules: true,
-        exports: 'auto',
       },
-      external: (id: string) => !id.startsWith('.') && !path.isAbsolute(id),
-    },
-    lib: {
-      fileName: (_format, entryName) => `${entryName}.js`,
-      entry: [path.resolve(__dirname, 'index.ts')],
-      name: 'eslint-plugin-de-morgan',
-      formats: ['cjs'],
     },
     minify: false,
   },
   plugins: [
     dts({
-      afterBuild: async () => {
-        await fs.writeFile(
-          'dist/index.d.ts',
-          `${(await fs.readFile('dist/index.d.ts'))
-            .toString()
-            .replace(/\nexport .+/u, '')}export = _default`,
-        )
-      },
       include: [
-        path.join(__dirname, 'index.ts'),
-        path.join(__dirname, 'rules'),
-        path.join(__dirname, 'utils'),
+        path.join(import.meta.dirname, 'index.ts'),
+        path.join(import.meta.dirname, 'rules'),
+        path.join(import.meta.dirname, 'utils'),
       ],
       insertTypesEntry: true,
       strictOutput: true,
-      rollupTypes: true,
+      copyDtsFiles: true,
     }),
     prettierFormat(),
   ],
